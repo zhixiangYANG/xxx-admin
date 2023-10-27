504b6d2d08dc4398a1089e88c2ca5009e33f4855
yangzhixiang <641383539@qq.com>
2023-10-27 11:42:24 +0800

    'feat:初始化配置'

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+declare module "*.vue" {
+  import type { DefindComponent } from "vue";
+  const component: DefindComponent<{}, {}, any>;
+  export default component;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 开发环境使用
+  server:{
+    port:8888,
+    open:true
+  },
   plugins: [
     vue(),
   ],
