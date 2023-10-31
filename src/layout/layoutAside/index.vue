8e3341faee039c479871918fe1e9024d712d319a
yangzhixiang <641383539@qq.com>
2023-10-31 16:25:21 +0800

    'feat:左侧菜单顶部logo'

--- src/layout/layoutAside/index.vue
+++ src/layout/layoutAside/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
+import { defineAsyncComponent } from 'vue';
+const Logo = defineAsyncComponent(() => import('./logo.vue'))
 </script>
 
 <template>
   <div class="h100">
-    <el-aside class="layout-aside layout-aside-menu-200">Aside</el-aside>
+    <el-aside class="layout-aside layout-aside-menu-200">
+      <Logo></Logo>
+    </el-aside>
   </div>
 </template>
 
---
+++ src/layout/layoutAside/logo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="layout-logo">
+  <img class="layout-logo-img" src="@/assets/logo.png" alt="">
+  <span>XXX-ADMIN</span>
+ </div>
+</template>
+
+<style></style>
