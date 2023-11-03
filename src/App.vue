359dcc54593da7234e4e083e332f7e0c1bd8215e
yangzhixiang <641383539@qq.com>
2023-11-03 11:02:12 +0800

    'feat:pinia保持布局状态持久化’

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
-
+import { useLayoutConfigStore } from '@/stores/layoutConfig';
+import { Local } from '@/utils/storage';
+import { onMounted } from 'vue';
+const layoutConfig = useLayoutConfigStore()
+onMounted(() => {
+  // 获取localstorage中的布局配置数据
+  const layoutConfigState = Local.get('layoutConfig')
+  // 更新到pinia状态中去
+  if (layoutConfigState) layoutConfig.updateState(layoutConfigState)
+})
 </script>
 
 <template>
--- src/stores/layoutConfig.ts
+++ src/stores/layoutConfig.ts
@@ -1,12 +1,29 @@
+import { Local } from "@/utils/storage";
 import { defineStore } from "pinia";
-
+import { nextTick } from "vue";
 export const useLayoutConfigStore = defineStore("layoutCongfig", {
   state: (): LayoutConfigState => {
     return {
       isCollapse: false, // 是否折叠菜单
-      globalTitle:'xxx-ADMIN',
-      isFullScreen:false,//是否全屏状态
-      isDrak:false,//是否暗黑模式
+      globalTitle: "xxx-ADMIN",
+      isFullScreen: false, //是否全屏状态
+      isDrak: false, //是否暗黑模式
     };
   },
+  actions: {
+    // 更新状态
+    updateState(state: LayoutConfigState) {
+      // 将传递的对象属性值更新到state状态中
+      this.$patch(state);
+    },
+  },
+});
+
+nextTick(() => {
+  const layoutConfig = useLayoutConfigStore();
+  // 监听状态变化，将状态持久化
+  layoutConfig.$subscribe((mutation, state) => {
+    // 保存到浏览器localStorage
+    Local.set("layoutConfig", state);
+  });
 });
