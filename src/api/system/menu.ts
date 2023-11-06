6b7d2d232f597fb8a7efe3e4c6de0dce8c13ad7d
yangzhixiang <641383539@qq.com>
2023-11-06 10:38:50 +0800

    'feat:table菜单树列表'

--- src/api/system/menu.ts
+++ src/api/system/menu.ts
@@ -1,8 +1,17 @@
 import request from "@/utils/request";
 
+const baseUrl ='/system/menu'
+
+
+/**
+ * 查询系统菜单列表
+ * @param query  查询条件 SysMenuQuery
+ * 
+ * @returns 
+ */
 export function getList(query:SysMenuQuery) {
   return request({
-    url: "/system/menu/search",
+    url: `${baseUrl}/search`,
     method: "POST",
     data: query,
   });
--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,13 +1,55 @@
 <template>
   <div>
-      菜单管理
+    <el-table ref="tableListRef" :data="tableList" stripe style="width: 100%" border v-loading="loading" row-key="id">
+      <el-table-column prop="meta.title" label="菜单名称" header-align="center" align="left">
+        <template #default="{ row }">
+          <SvgIcon :name="row.meta?.icon || ''"></SvgIcon>
+          <span class="ml10">{{ row.meta?.title }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="path" label="路由地址" header-align="center" align="left" show-overflow-tooltip />
+      <el-table-column prop="component" label="组件名称" header-align="center" align="left" show-overflow-tooltip />
+      <el-table-column prop="code" label="权限标识" header-align="center" align="left" show-overflow-tooltip />
+      <el-table-column prop="type" label="类型" align="center">
+        <template #default="{ row }">
+          <el-tag effect="plain" :type="row.type == 1 ? '' : 'success'">{{ row.type == '1' ? '菜单' : '按钮' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="sort" label="排序" align="center" />
+      <el-table-column label="操作" align="center"></el-table-column>
+    </el-table>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts" name="SystemMenu">
+import { getList } from '@/api/system/menu';
+import { onMounted, reactive, toRefs } from 'vue';
 
+const state = reactive({
+  loading: false,
+  query: {
+    keyword: '',
+  } as SysMenuQuery,
+  tableList: [] as SysMenuType[]
+})
+
+const { loading, query, tableList } = { ...toRefs(state) }
+
+onMounted(() => {
+  queryData()
+})
+
+async function queryData() {
+  try {
+    state.loading = true
+    const { data } = await getList(state.query)
+    console.log(data)
+    state.tableList = data
+  } catch (error) {
+  } finally {
+    state.loading = false
+  }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
